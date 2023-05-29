b8de691ff9880817b3a5e9d58183bb4ee20586fd
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2023-05-29 16:22:53 +0200

    minor changes in studentA

--- studentA.py
+++ studentA.py
@@ -14,31 +14,38 @@ def print_board(board):
     print('--------------------------')
 
 def is_game_over(board):
-
+    # first diagonal
     flag = True
     for i in range(0, 5):
         if board[i][i] is not board[0][0]:
             flag = False
-    if flag is True:
+    if flag:
         return True
+
+    # second diagonal
     flag = True
     for i in range(0, 5):
         if board[i][i] is not board[4][0]:
             flag = False
-    if flag is True:
+    if flag:
         return True
     
-
+    # columns, idk if u can do this in smarter way but at least works
     for x in range(0, 5):
         flag = True
         for y in range(0, 5):
             if board[y][x] != board[0][x]:
                 flag = False
-        if flag is True and board[0][x] != ' ':
+        if flag and board[0][x] != ' ':
             return True
     
+    # check if all elements
     for y in range(0, 5):
-        return all(board[y][0] == elem for elem in board[y])
+        result = board[y][0] == elem for elem in board[y])
+        if result and board[y][0] != ' ':
+            return True
+
+    return False
 
 print(is_game_over(b))
 
